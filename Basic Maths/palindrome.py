41cd725c9d8e3c551ef5b2d8a3c299ab3619384d
Anjaneyulu143 <vinayanjaneyulu2@gmail.com>
2022-07-28 23:51:41 +0530

    Finding the given number is a palindrome or not

---
+++ Basic Maths/palindrome.py
@@ -0,0 +1,21 @@
+def palindrome(n):
+    temp = n
+    reverse_digit = 0
+    
+    while (n>0):
+        rem = n%10
+        reverse_digit = reverse_digit * 10 + rem
+        n //= 10
+    if (temp == reverse_digit):
+        return True
+    else:
+        return False
+
+n = int(input())
+
+result = palindrome(n)
+
+if result:
+    print("Given number is a palindrome")
+else:
+    print("Given number is not a palindrome")
